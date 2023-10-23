ec15d9f3caa512a16d7a5a62ceb5a28a38d0bc8c
pedrojunior079 <pedroarnsjr@outlook.com>
2023-10-23 15:23:47 -0300

    Curso Nodejs Aula 04 Criar Rotas POST

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ import express from 'express';
 
 const app = express();
 
+// inidcar para o express ler o body com json
+app.use(express.json());
+
 // mock
-const frameworks = [
-   {id: 1, frameworks: 'Laravel'},
-   {id: 2, frameworks: 'Node'},
-   {id: 3, frameworks: 'Vue'},
-   {id: 4, frameworks: 'React'},
-   {id: 5, frameworks: 'Angular'}
+const linguagensDeProg = [
+   {id: 1, linguagensDeProg: 'Java'},
+   {id: 2, linguagensDeProg: 'C#'},
+   {id: 3, linguagensDeProg: 'PHP'},
+   {id: 4, linguagensDeProg: 'HTML5'},
+   {id: 5, linguagensDeProg: 'CSS3'},
+   {id: 6, linguagensDeProg: 'JavaScript'}
 ] 
 
 
@@ -17,8 +21,13 @@ app.get('/', (req, res) => {
    res.send('Olá mundo, bem vindo ao Curso de Nodejs!');
 });
 
-app.get('/frameworks', (req, res) => {
-   res.status(200).send(frameworks);
+app.get('/linguagensDeProg', (req, res) => {
+   res.status(200).send(linguagensDeProg);
+});
+
+app.post('/linguagensDeProg', (req, res) => {
+   frameworks.push(req.body);
+   res.status(201).send('Framework cadastrado com sucesso!');
 });
 
 export default app;
