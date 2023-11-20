e171d9dc86d10088653c1f7dac59315ce92f6cf1
pedrojunior079 <pedroarnsjr@outlook.com>
2023-11-20 18:53:54 -0300

    Curso NodeJs Aula07 Persistencia de Dados

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import app from "./src/app.js";
+import conexao from './infra/conexao.js';
 const PORT = 3000;
 
-// escutar a porta 3000
-app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${PORT}`); 
- })
+//fazer a conexão
+conexao.connect((erro) => {
+    if(erro){
+       console.log(erro); 
+    }else{
+       console.log("Conexão relizada com sucesso!");
+       // escutar a porta 3000
+       app.listen(PORT, () => {
+           console.log(`Servidor rodando no endereço http://localhost:${PORT}`); 
+       });  
+    }
+});
+
